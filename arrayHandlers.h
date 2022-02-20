e5ae5cb8bf7148dbcf2b9dc45ad860932f323e9a
danPr0 <94548001+danPr0@users.noreply.github.com>
2022-02-21 01:17:14 +0200

    Update arrayHandlers.h

--- arrayHandlers.h
+++ arrayHandlers.h
@@ -2,4 +2,4 @@ void fillArrayWithRandom(int* &a, int &size, int n);
 
 void fillArrayWithKeyboard(int* &a, int &size, int n);
 
-void arrayOutput(int* a, int size);
+void arrayOutput(int* a, int size, std::string task);
